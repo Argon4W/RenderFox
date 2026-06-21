3befb65fe14859e4777863ae85d1ef5975cfca7c
Argon4W <1516775492@qq.com>
2026-06-22 02:10:27 +0800

    Add license headers for new files

--- src/main/java/com/github/argon4w/renderfox/opengl/framebuffer/function/GLFramebufferFunctionsValidation.java
+++ src/main/java/com/github/argon4w/renderfox/opengl/framebuffer/function/GLFramebufferFunctionsValidation.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026  Argon4W
+ *
+ * This file is part of RenderFox.
+ *
+ * RenderFox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RenderFox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RenderFox.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.github.argon4w.renderfox.opengl.framebuffer.function;
 
 import com.github.argon4w.renderfox.data.UnionValue;
